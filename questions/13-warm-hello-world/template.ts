cbb985848af8249118a19f5c8d99de46eacee1fd
PangTFish <670739394@qq.com>
2022-02-13 21:50:57 +0800

    🐞 fix: 13-warm-hello-world

--- questions/13-warm-hello-world/template.ts
+++ questions/13-warm-hello-world/template.ts
@@ -1,1 +1,9 @@
-type HelloWorld = any // expected to be a string
+/*
+ * @Description: 
+ * @version: 
+ * @Author: 初柒
+ * @Date: 2022-02-07 21:20:45
+ * @LastEditors: 初柒
+ * @LastEditTime: 2022-02-13 21:48:21
+ */
+type HelloWorld = string // expected to be a string
